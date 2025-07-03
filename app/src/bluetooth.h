ec0a242eb905e43846b249a4d6353295e6594235
Harmeet Singh <harmeet@golfdiscount.com>
2025-07-03 00:42:15 -0700

    Added definitions for threads and a struct definition for tracking scan observations

--- app/src/bluetooth.h
+++ app/src/bluetooth.h
@@ -1,9 +1,34 @@
-#define BT_THREAD_STACK_SIZE 1024
-#define BT_THREAD_PRIORITY 5
+// Device name cannot be longer than 30 due to the size of a packet
+#define BT_MAX_DEVICE_NAME_LEN 30
+#define BT_PROCESS_THREAD_STACK_SIZE 1024
+#define BT_PROCESS_THREAD_PRIORITY 4
+#define BT_SCAN_THREAD_STACK_SIZE 1024
+#define BT_SCAN_THREAD_PRIORITY 5
 
+/**
+ * @brief A struct representing Bluetooth advertisements from
+ * the Bluetooth module
+ */
 struct bt_scan_result {
     char *addr_str;
     int8_t rssi;
     uint8_t type;
     struct net_buf_simple *ad;
+};
+
+/**
+ * @brief A struct representing a singular Bluetooth observations.
+ * 
+ * surrogate_key should be a randomly generated uint8_t generated at
+ * boot to function as a unqiue identifer for a series of observations.
+ * 
+ * surrogate_key and sys_tick_elapsed sufficiently identifies a unique slice
+ * of time for an observation.
+ */
+struct bt_scan_obvs {
+    char *addr;
+    char *device_name;
+    int8_t rssi;
+    uint8_t surrogate_key;
+    int64_t sys_tick_elapsed;
 };
