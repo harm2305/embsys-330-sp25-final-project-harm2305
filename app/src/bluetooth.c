f75078eaca592fd939ed2a77779f1d608971d99d
Harmeet Singh <harmeet@golfdiscount.com>
2025-07-03 01:24:16 -0700

    Completed setup of two threads to manage scan state and processing scan results

--- app/src/bluetooth.c
+++ app/src/bluetooth.c
@@ -6,15 +6,17 @@
 
 LOG_MODULE_REGISTER(bt_scanner, LOG_LEVEL_DBG);
 
+static bool extract_bt_device_name_cb(struct bt_data *data, void *user_data);
+static void bt_process(void *, void *, void *);
 static void bt_scan(void *, void *, void *);
 static void bt_scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t type, struct net_buf_simple *ad);
-static bool extract_bt_device_name_cb(struct bt_data *data, void *user_data);
 
 K_EVENT_DEFINE(toggle_scan_type_event);
-K_MSGQ_DEFINE(bt_scan_msgq, sizeof(uint8_t), 1, 1);
+K_MSGQ_DEFINE(bt_process_msgq, sizeof(struct bt_scan_result), 16, 1);
 
 K_THREAD_DEFINE(bt_scan_thread_tid, BT_SCAN_THREAD_STACK_SIZE, bt_scan, NULL, NULL, NULL, BT_SCAN_THREAD_PRIORITY, 0, 0);
-K_THREAD_STACK_DEFINE(bt_thread_stack_area, BT_SCAN_THREAD_STACK_SIZE);
+K_THREAD_DEFINE(bt_process_thread_tid, BT_PROCESS_THREAD_STACK_SIZE, bt_process, NULL, NULL, NULL, BT_PROCESS_THREAD_PRIORITY, 0, 0);
+
 
 /**
  * @brief Enables Bluetooth, initializes callbacks, and begins scanning for devices.
@@ -77,61 +79,62 @@ static void bt_scan(void *, void *, void *) {
 
 /**
  * Callback that processes a Bluetooth scan result
- * and submits to a queue for processing later
+ * and submits to a queue for processing later in a separate thread.
  */
 static void bt_scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t type, struct net_buf_simple *ad) {
 	char addr_str[BT_ADDR_LE_STR_LEN];
-	char name[BT_MAX_DEVICE_NAME_LEN];
+	char name[BT_MAX_DEVICE_NAME_LEN] = {0};
 
 	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
 	bt_data_parse(ad, extract_bt_device_name_cb, name);
 
-	// LOG_INF("Device found: %s with address %s (RSSI %d), type %u, AD data len %u",
-	// 	name, addr_str, rssi, type, ad->len);
-
-	// struct bt_scan_result scan_result;
+	struct bt_scan_result scan_result;
 	
-	// scan_result.addr_str = addr_str;
-	// scan_result.rssi = rssi;
-	// scan_result.type = type;
-	// scan_result.ad = ad;
+	scan_result.addr_str = addr_str;
+	scan_result.rssi = rssi;
+	scan_result.type = type;
+	scan_result.ad = ad;
 
-	// LOG_DBG("Queuing an item to the Bluetooth item queue");
+    strcpy(scan_result.device_name, name);
 
-	// int ret = k_msgq_put(&bt_msgq, &scan_result, K_FOREVER);
+	LOG_DBG("Queuing an item to the Bluetooth item queue");
+
+	int ret = k_msgq_put(&bt_process_msgq, &scan_result, K_FOREVER);
+
+    if (ret) {
+        LOG_ERR("Error %d: Failed to queue a scan result for processing later", ret);
+    }
 }
 
-// static void process_scan_result(void *, void *, void *) {
+/**
+ * @brief Processes an incoming Bluetooth scan result
+ */
+static void bt_process(void *, void *, void *) {
 
-//     while (1) {
-//         struct bt_scan_result scan_result;
+    while (1) {
+        struct bt_scan_result scan_result;
 
-//         if (k_msgq_get(&bt_scan_msgq, &scan_result, K_NO_WAIT) == 0) {
-//             char name[BT_MAX_DEVICE_NAME_LEN];
+        if (k_msgq_get(&bt_process_msgq, &scan_result, K_FOREVER) == 0) {
+            LOG_INF("Device found: %s with address %s (RSSI %d), type %u, AD data len %u",
+	           scan_result.device_name, scan_result.addr_str, scan_result.rssi, scan_result.type, scan_result.ad->len);
+        }
+    }
+}
 
-//             struct net_buf_simple *ad = scan_result.ad;
-
-//             bt_data_parse(ad, extract_bt_device_name_cb, name);
-
-//             // LOG_INF("Device found: %s with address %s (RSSI %d), type %u, AD data len %u\n",
-// 	        //    name, scan_result.addr_str, scan_result.rssi, scan_result.type, scan_result.ad->len);
-//         }
-
-//         k_msleep(100);
-//     }
-// }
-
+/**
+ * Processes the Bluetooth data buffer of a scan and
+ * retrieves the name, if it is known.
+ */
 static bool extract_bt_device_name_cb(struct bt_data *data, void *user_data) {
     char *name = user_data;
 
     if (data->type != BT_DATA_NAME_COMPLETE && data->type != BT_DATA_NAME_SHORTENED) {
+        strcpy(name, "unknown");
         return false;
     }
 
     size_t len = MIN(data->data_len, BT_MAX_DEVICE_NAME_LEN - 1);
     memcpy(name, data->data, len);
-    // Manually terminate the string to account for strings shorter than BT_MAX_DEVICE_NAME_LEN - 1 
-    name[len] = '\0';
 
     return true;
 }
