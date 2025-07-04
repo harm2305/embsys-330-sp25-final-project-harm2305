b8e9af26a9134f2516ff0fa56dd5bddaac27fa74
Harmeet Singh <harmeet@golfdiscount.com>
2025-07-03 22:20:40 -0700

    Removed skeleton code and added text to indicate current scan mode

--- app/src/main.c
+++ app/src/main.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/display.h>
@@ -16,6 +17,14 @@
 
 #include "bluetooth.h"
 #include "counter.h"
+
+#define COLOR_RED 0xFF0000
+#define COLOR_GREEN 0x00FF00
+#define COLOR_GRAY 0x2D3639
+#define COLOR_LIGHT_GRAY 0x6F8187
+
+static void set_default_box_styling(lv_obj_t *box);
+static void set_scan_label(lv_obj_t *label, bool scan_mode);
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
@@ -47,19 +56,11 @@ static const struct device *lvgl_keypad =
 	DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_keypad_input));
 #endif /* CONFIG_LV_Z_KEYPAD_INPUT */
 
-static void lv_btn_click_callback(lv_event_t *e)
-{
-	ARG_UNUSED(e);
-
-	counter_reset();
-}
-
 int main(void)
 {
-	char count_str[11] = {0};
 	const struct device *display_dev;
-	lv_obj_t *hello_world_label;
-	lv_obj_t *count_label;
+	lv_coord_t screen_width = lv_disp_get_hor_res(NULL);
+	lv_coord_t screen_height = lv_disp_get_ver_res(NULL);
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(display_dev)) {
@@ -121,34 +122,55 @@ int main(void)
 	lv_indev_set_group(lvgl_input_get_indev(lvgl_keypad), btn_matrix_group);
 #endif /* CONFIG_LV_Z_KEYPAD_INPUT */
 
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
-		lv_obj_t *hello_world_button;
+	const lv_coord_t top_bar_height = screen_height * 0.15;
+	// Create and style top status bar
+	lv_obj_t *top_bar = lv_obj_create(lv_scr_act());
+	lv_obj_set_size(top_bar, screen_width, top_bar_height);
+	lv_obj_set_style_bg_color(top_bar, lv_color_hex(COLOR_GRAY), 0);
+	set_default_box_styling(top_bar);
+	lv_obj_align(top_bar, LV_ALIGN_TOP_LEFT, 0, 0);
 
-		hello_world_button = lv_btn_create(lv_scr_act());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
-		lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
-				    NULL);
-		hello_world_label = lv_label_create(hello_world_button);
-	} else {
-		hello_world_label = lv_label_create(lv_scr_act());
-	}
+	// Create and style main content box
+	lv_obj_t *main = lv_obj_create(lv_scr_act());
+	lv_obj_set_size(main, screen_width, screen_height - top_bar_height);
+	lv_obj_set_style_bg_color(main, lv_color_hex(COLOR_LIGHT_GRAY), 0);
+	set_default_box_styling(main);
+	lv_obj_align(main, LV_ALIGN_TOP_LEFT, 0, top_bar_height);
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
-
-	count_label = lv_label_create(lv_scr_act());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+	lv_obj_t *scan_label = lv_label_create(lv_scr_act());
+	lv_obj_align(scan_label, LV_ALIGN_TOP_LEFT, 5, 10);
 
 	lv_task_handler();
 	display_blanking_off(display_dev);
 
 	while (1) {
-		if ((counter_value() % 100) == 0U) {
-			sprintf(count_str, "%d", counter_value() / 100U);
-			lv_label_set_text(count_label, count_str);
-		}
-		counter_increment();
+		bool scan_mode = get_current_scan_mode();
+		set_scan_label(scan_label, scan_mode);
 		lv_task_handler();
 		k_sleep(K_MSEC(10));
+	}
+}
+
+/**
+ * Removes all padding, margin, and radius styling on an object.
+ * This is meant primary for boxes.
+ */
+static void set_default_box_styling(lv_obj_t *box) {
+	lv_obj_set_style_pad_all(box, 0, 0);
+	lv_obj_set_style_border_width(box, 0, 0);
+	lv_obj_set_style_radius(box, 0, 0);
+}
+
+/**
+ * Sets the content and styles the scan label displayed
+ * at the top bar.
+ */
+static void set_scan_label(lv_obj_t *label, bool scan_mode) {
+	if (scan_mode == true) {
+		lv_label_set_text(label, "ACTIVE");
+		lv_obj_set_style_text_color(label, lv_color_hex(COLOR_GREEN), 0);
+	} else {
+		lv_label_set_text(label, "PASSIVE");
+		lv_obj_set_style_text_color(label, lv_color_hex(COLOR_RED), 0);
 	}
 }
